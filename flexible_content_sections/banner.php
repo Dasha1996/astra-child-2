85a20c591c8b52cf0b5fbcaa7cf6ac6ebbf84972
dasha-gurevichpwdpwdpwd <dashawebdev96@gmail.com>
2024-01-06 18:59:27 +0000

    commit

--- flexible_content_sections/banner.php
+++ flexible_content_sections/banner.php
@@ -9,7 +9,7 @@ $button_link = get_sub_field('button_link');
 ?><div class="banner-section section">
     <div class="container-custom">
         <div class="d-flex text-image-layout">
-    <div class="col text-col d-flex">
+    <div class="col text-col d-flex justify-content-center">
         <div class="text-container d-flex flex-column">
             <?if(!empty($subtitle)) {
                 ?>
